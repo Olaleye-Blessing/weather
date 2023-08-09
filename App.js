462958711b41aa4d684d128bbee44cc773762106
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-08-09 05:24:31 +0100

    Learn about Views and Text

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text>Current Weather</Text>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>It's sunny</Text>
+        <Text style={styles.message}>It's perfect T-shirt weather</Text>
+      </View>
+    </SafeAreaView>
+  )
 }
 
+// when using flex 1, all components up the tree must have flex 1
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'pink'
   },
-});
+  container: {
+    flex: 1, // fill the entire screen,
+    alignItems: 'center'
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels: {
+    color: 'black',
+    fontSize: 30
+  },
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20
+  },
+  bodyWrapper: {
+    alignItems: 'center'
+  },
+  description: {
+    fontSize: 28
+  },
+  message: {
+    fontSize: 20
+  }
+})
