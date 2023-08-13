d1dec88710d4bb800722b8ee2fbf40bb0df9a8ef
Olaleye Blessing <Olayinkablexxy@gmail.com>
2023-08-13 12:01:45 +0100

    Get location
    - This should be fixed later since the package to get location isn't
    working properly on expo.

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
+import { useEffect, useState } from 'react'
+import { View, StyleSheet, ActivityIndicator } from 'react-native'
+import * as Location from 'expo-location'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
+import { myLocation } from './src/utilities/myLocation'
 
 export default function App() {
+  const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    ;(async () => {
+      // Assume that location permission has been granted
+      setLocation(myLocation)
+    })()
+  }, [])
+
+  if (loading)
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="blue" />
+      </View>
+    )
+
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
---
+++ src/utilities/myLocation.js
@@ -0,0 +1,14 @@
+// TODO: use geolocation to get user's location from expo-location. This is a mock location. expo-location is not working on my machine
+export const myLocation = {
+  coords: {
+    accuracy: 5,
+    altitude: 0,
+    altitudeAccuracy: -1,
+    heading: -1,
+    // Lagos Nigeria
+    latitude: 6.5243793,
+    longitude: 3.3792057,
+    speed: -1
+  },
+  timestamp: 1600000000000
+}
